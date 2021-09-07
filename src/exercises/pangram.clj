4b3f4aa32f1396ac237086547333914fa1e7996c
TUltra <thiago_ultra@yahoo.com.br>
2021-09-07 11:13:55 -0300

    pangram solved

---
+++ src/exercises/pangram.clj
@@ -0,0 +1,22 @@
+(ns exercises.pangram)
+
+(def alphabet "abcdefghijklmnopqrstuvwxyz")
+
+(defn alphabet-freq [alpha]
+  (let [letters (seq alpha)
+        freq nil]
+       (apply merge (map #(assoc freq % 0) letters))))
+
+(defn pangram? [phrase] ;; <- arglist goes here
+  ;; your code goes here
+  (let [x (merge (alphabet-freq alphabet) (frequencies (clojure.string/lower-case phrase)))
+        y (filter #(= (second %) 0) x)]
+        (if (> (count y) 0) false true)
+    )
+)
+
+;; (pangram? "the quick brown fox jumps over the lazy dog")
+(pangram? "the quick brown fox jumps over the lazy ")
+
+;; (alphabet)
+;;
